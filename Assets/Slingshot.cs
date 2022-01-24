5ec91ae1501204e5b761f73f3940957537aa5233
Sami <draqgamedev@gmail.com>
2022-01-24 03:04:26 +0300

    base
    Base project with functional slingshot

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public LineRenderer frontRope;
+    public LineRenderer backRope;
+
+    public Transform bird;
+    public Transform slingshotTarget;
+    public Transform center;
+
+    public float throwForce;
+    public float maxRopeSize;
+
+    private Rigidbody2D birdRigidbody;
+    private Vector2 targetLastPos;
+
+    void Start()
+    {
+        //Setup Slingshot
+        frontRope.positionCount = 2;
+        backRope.positionCount = 2;
+        frontRope.SetPosition(0, frontRope.transform.position);
+        backRope.SetPosition(0, backRope.transform.position);
+    }
+
+    void Update()
+    {
+        //Yayýn iplerinin pozisyonunu günceller
+        frontRope.SetPosition(1, slingshotTarget.position);
+        backRope.SetPosition(1, slingshotTarget.position);
+
+        if (Input.GetMouseButton(0))
+        {
+            UpdateSlingshot();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            birdRigidbody = bird.GetComponent<Rigidbody2D>();
+            birdRigidbody.isKinematic = false;
+            birdRigidbody.AddForce((slingshotTarget.position - center.position) * throwForce * -10);
+            targetLastPos = slingshotTarget.localPosition;
+            StartCoroutine(ResetSlingshot());
+        }
+    }
+
+    private IEnumerator ResetSlingshot()
+    {
+        float resetTime = 0.1f;
+        float elapsedTime = 0;
+
+        while (elapsedTime < resetTime)
+        {
+            elapsedTime += Time.deltaTime;
+            slingshotTarget.localPosition = Vector3.Lerp(targetLastPos, Vector3.zero, elapsedTime / resetTime);
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    private void UpdateSlingshot()
+    {
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition = Vector3.ClampMagnitude(targetPosition - center.position, maxRopeSize);
+        slingshotTarget.localPosition = targetPosition;
+
+        //Update Bird Transform
+        Vector2 direction = slingshotTarget.position - center.position;
+        float angle = Mathf.Atan2(direction.y, direction.x);
+        bird.position = slingshotTarget.position - (new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) / 4);
+        bird.rotation = Quaternion.AngleAxis((angle * Mathf.Rad2Deg) - 180f, Vector3.forward);
+    }
+}
